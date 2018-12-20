df7436143e6145f634535108fb46c8d599cc33f9
aavilaco <aavilaco@gmail.com>
2018-12-20 17:47:21 +0100

    Update install-agent.ps1

--- install-agent.ps1
+++ install-agent.ps1
@@ -21,8 +21,8 @@ Start-Sleep -s 5
 .\Setup.exe /qn AcceptEndUserLicenseAgreement=1 ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_ID=$WORKSPACE_ID OPINSIGHTS_WORKSPACE_KEY=$WORKSPACE_KEY OPINSIGHTS_WORKSPACE_AZURE_CLOUD_TYPE=0 
 
 wget https://download.sysinternals.com/files/Sysmon.zip -OutFile Sysmon.zip
-Unzip "$pwd\Sysmon.zip" "$pwd\sysmon"
-cd sysmon
+Unzip "$pwd\Sysmon.zip" "C:\sysmon"
+cd C:\sysmon
 
 wget https://raw.githubusercontent.com/aavilaco/practica-logs/master/sysmon-config.xml -OutFile sysmon-config.xml
 .\sysmon.exe -accepteula -i sysmon-config.xml
